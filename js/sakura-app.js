91ff5fab535c424a22ecc6446d328b38f2e0e1de
juezho <1499475022@qq.com>
2020-09-27 10:15:49 +0800

    Site updated: 2020-09-27 10:15:02

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -1399,94 +1399,94 @@ var home = location.href,
         $('#main-container,#mo-nav,.openNav').toggleClass('open')
       }
     },
-    splay: function () {
-      $('#video-btn').addClass('video-pause').removeClass('video-play').show()
-      $('.video-stu').css({
-        'bottom': '-100px'
-      })
-      $('.focusinfo').css({
-        'top': '-999px'
-      })
-      // for (var i = 0; i < ap.length; i++) {
-      //     try {
-      //         ap[i].destroy()
-      //     } catch (e) {}
-      // }
-      // try {
-      //     hermitInit()
-      // } catch (e) {}
-      s.play()
-    },
-    spause: function () {
-      $('#video-btn').addClass('video-play').removeClass('video-pause')
-      $('.focusinfo').css({
-        'top': '49.3%'
-      })
-      s.pause()
-    },
-    liveplay: function () {
-      if (s.oncanplay != undefined && $('.haslive').length > 0) {
-        if ($('.videolive').length > 0) {
-          Siren.splay()
-        }
-      }
-    },
-    livepause: function () {
-      if (s.oncanplay != undefined && $('.haslive').length > 0) {
-        Siren.spause()
-        $('.video-stu').css({
-          'bottom': '0px'
-        }).html('已暂停 ...')
-      }
-    },
-    addsource: function () {
-      $('.video-stu').html('正在载入视频 ...').css({
-        'bottom': '0px'
-      })
-      var t = Poi.movies.name.split(','),
-        _t = t[Math.floor(Math.random() * t.length)]
-      $('#bgvideo').attr('src', Poi.movies.url + '/' + _t)
-      $('#bgvideo').attr('video-name', _t)
-    },
-    LV: function () {
-      var _btn = $('#video-btn')
-      _btn.on('click', function () {
-        if ($(this).hasClass('loadvideo')) {
-          $(this).addClass('video-pause').removeClass('loadvideo').hide()
-          Siren.addsource()
-          s.oncanplay = function () {
-            Siren.splay()
-            $('#video-add').show()
-            _btn.addClass('videolive')
-            _btn.addClass('haslive')
-          }
-        } else {
-          if ($(this).hasClass('video-pause')) {
-            Siren.spause()
-            _btn.removeClass('videolive')
-            $('.video-stu').css({
-              'bottom': '0px'
-            }).html('已暂停 ...')
-          } else {
-            Siren.splay()
-            _btn.addClass('videolive')
-          }
-        }
-        s.onended = function () {
-          $('#bgvideo').attr('src', '')
-          $('#video-add').hide()
-          _btn.addClass('loadvideo').removeClass('video-pause')
-          _btn.removeClass('videolive')
-          _btn.removeClass('haslive')
-          $('.focusinfo').css({
-            'top': '49.3%'
-          })
-        }
-      })
-      $('#video-add').on('click', function () {
-        Siren.addsource()
-      })
-    },
+    // splay: function () {
+    //   $('#video-btn').addClass('video-pause').removeClass('video-play').show()
+    //   $('.video-stu').css({
+    //     'bottom': '-100px'
+    //   })
+    //   $('.focusinfo').css({
+    //     'top': '-999px'
+    //   })
+    //   // for (var i = 0; i < ap.length; i++) {
+    //   //     try {
+    //   //         ap[i].destroy()
+    //   //     } catch (e) {}
+    //   // }
+    //   // try {
+    //   //     hermitInit()
+    //   // } catch (e) {}
+    //   s.play()
+    // },
+    // spause: function () {
+    //   $('#video-btn').addClass('video-play').removeClass('video-pause')
+    //   $('.focusinfo').css({
+    //     'top': '49.3%'
+    //   })
+    //   s.pause()
+    // },
+    // liveplay: function () {
+    //   if (s.oncanplay != undefined && $('.haslive').length > 0) {
+    //     if ($('.videolive').length > 0) {
+    //       Siren.splay()
+    //     }
+    //   }
+    // },
+    // livepause: function () {
+    //   if (s.oncanplay != undefined && $('.haslive').length > 0) {
+    //     Siren.spause()
+    //     $('.video-stu').css({
+    //       'bottom': '0px'
+    //     }).html('已暂停 ...')
+    //   }
+    // },
+    // addsource: function () {
+    //   $('.video-stu').html('正在载入视频 ...').css({
+    //     'bottom': '0px'
+    //   })
+    //   var t = Poi.movies.name.split(','),
+    //     _t = t[Math.floor(Math.random() * t.length)]
+    //   $('#bgvideo').attr('src', Poi.movies.url + '/' + _t)
+    //   $('#bgvideo').attr('video-name', _t)
+    // },
+    // LV: function () {
+    //   var _btn = $('#video-btn')
+    //   _btn.on('click', function () {
+    //     if ($(this).hasClass('loadvideo')) {
+    //       $(this).addClass('video-pause').removeClass('loadvideo').hide()
+    //       Siren.addsource()
+    //       s.oncanplay = function () {
+    //         Siren.splay()
+    //         $('#video-add').show()
+    //         _btn.addClass('videolive')
+    //         _btn.addClass('haslive')
+    //       }
+    //     } else {
+    //       if ($(this).hasClass('video-pause')) {
+    //         Siren.spause()
+    //         _btn.removeClass('videolive')
+    //         $('.video-stu').css({
+    //           'bottom': '0px'
+    //         }).html('已暂停 ...')
+    //       } else {
+    //         Siren.splay()
+    //         _btn.addClass('videolive')
+    //       }
+    //     }
+    //     s.onended = function () {
+    //       $('#bgvideo').attr('src', '')
+    //       $('#video-add').hide()
+    //       _btn.addClass('loadvideo').removeClass('video-pause')
+    //       _btn.removeClass('videolive')
+    //       _btn.removeClass('haslive')
+    //       $('.focusinfo').css({
+    //         'top': '49.3%'
+    //       })
+    //     }
+    //   })
+    //   $('#video-add').on('click', function () {
+    //     Siren.addsource()
+    //   })
+    // },
     AH: function () {
       if (Poi.windowheight == 'auto') {
         if ($('h1.main-title').length > 0) {
@@ -1514,7 +1514,7 @@ var home = location.href,
           $('.headertop').css({
             'height': 'auto'
           }).show()
-          if (Poi.movies.live == 'open') Siren.liveplay()
+          // if (Poi.movies.live == 'open') Siren.liveplay()
           $('.site-header').addClass('is-homepage')
         } else {
           $('.blank').css({
@@ -1523,7 +1523,7 @@ var home = location.href,
           $('.headertop').css({
             'height': '0px'
           }).hide()
-          Siren.livepause()
+          // Siren.livepause()
         }
       }
     },
@@ -1808,7 +1808,7 @@ $(function () {
   Siren.CE()
   Siren.MN()
   Siren.IA()
-  Siren.LV()
+  // Siren.LV()
   if (window.is_app) injectStyles('#nprogress .bar { display: none; }')
   if (Poi.pjax) {
     $(document).pjax('a[target!=_top]', '#page', {
--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // 加载 CSS
 $("<link>").attr({
-    href: "/live2d/waifu.css",
+    href: "//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu.css",
     rel: "stylesheet",
     type: "text/css"
 }).appendTo('head');
@@ -10,36 +10,41 @@ $('body').append('<div class="prpr"><div class="waifu-tips"></div><canvas id="li
 
 // 加载 JS
 $.ajax({
-    url: '/live2d/waifu-tips.min.js',
+    url: '//cdn.jsdelivr.net/gh/juezho/cdn/live2d/live2d.min.js',
     dataType: "script",
     cache: true,
     async: false
 });
 $.ajax({
-    url: '/live2d/live2d.min.js',
+    url: '//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu-tips.min.js',
     dataType: "script",
     cache: true,
     async: false
 });
-// 初始化看板娘，加载 waifu-tips.json
-
-/* 可直接修改部分参数 */
-live2d_settings['modelId'] = 1; // 默认模型 ID
-live2d_settings['modelTexturesId'] = 87; // 默认材质 ID
-live2d_settings['modelStorage'] = true; // 储存模型 ID
-live2d_settings['modelAPI'] = '//live2d.fghrsh.net/api/'; // 自建 API 修改这里
-live2d_settings['hitokotoAPI'] = '\fghrsh.net'; // 一言 API，可选 'lwl12.com', 'hitokoto.cn', 'jinrishici.com'(古诗词)
-live2d_settings['waifuEdgeSide'] = 'left:30'; // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
-live2d_settings['waifuDraggable'] = 'disable'; // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
-live2d_settings['homePageUrl'] = '/'; // 主页地址，可选 'auto'(自动), '{URL 网址}'
-live2d_settings['showToolMenu'] = false;         // 显示 工具栏          ，可选 true(真), false(假)
-// live2d_settings['canTakeScreenshot'] = false; // 显示 看板娘截图  按钮，可选 true(真), false(假)
-// live2d_settings['canTurnToAboutPage'] = false; // 显示 跳转关于页  按钮，可选 true(真), false(假)
-// live2d_settings['canSwitchModel'] = false; // 显示 模型切换    按钮，可选 true(真), false(假)
-
-/* 在 initModel 前添加 */
-initModel('/live2d/waifu-tips.json');
 
 
-
-
+!function live2d_try() {
+    // 初始化看板娘，加载 waifu-tips.json
+    try {
+        live2d_settings && loadlive2d;
+        /* 可直接修改部分参数 */
+        live2d_settings['modelId'] = 1; // 默认模型 ID
+        live2d_settings['modelTexturesId'] = 87; // 默认材质 ID
+        live2d_settings['modelStorage'] = true; // 储存模型 ID
+        live2d_settings['modelAPI'] = '//live2d.fghrsh.net/api/'; // 自建 API 修改这里
+        live2d_settings['hitokotoAPI'] = '\fghrsh.net'; // 一言 API，可选 'lwl12.com', 'hitokoto.cn', 'jinrishici.com'(古诗词)
+        live2d_settings['waifuEdgeSide'] = 'left:30'; // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
+        live2d_settings['waifuDraggable'] = 'disable'; // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
+        live2d_settings['homePageUrl'] = '/'; // 主页地址，可选 'auto'(自动), '{URL 网址}'
+        live2d_settings['showToolMenu'] = false; // 显示 工具栏          ，可选 true(真), false(假)
+        live2d_settings['canTakeScreenshot'] = false; // 显示 看板娘截图  按钮，可选 true(真), false(假)
+        live2d_settings['canTurnToAboutPage'] = false; // 显示 跳转关于页  按钮，可选 true(真), false(假)
+        live2d_settings['canSwitchModel'] = false; // 显示 模型切换    按钮，可选 true(真), false(假)
+        /* 在 initModel 前添加 */
+        initModel('//cdn.jsdelivr.net/gh/juezho/cdn/live2d/waifu-tips.json');
+    } catch {
+        setTimeout(function () {
+            live2d_try()
+        }, 5000)
+    }
+}()
